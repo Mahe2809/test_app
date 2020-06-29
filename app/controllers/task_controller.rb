814b5ce4c2bbb5aec96be1077adcfa6ed3f09356
Mahe2809 <mahe2809pro@gmail.com>
2020-06-29 19:09:20 +0530

    Value add in form

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -6,4 +6,16 @@ class TaskController < ApplicationController
     def index 
         @tasks = Task.all
     end
+    def new
+
+    end
+    def create
+        @task = Task.new(params.require(:task).permit(:task, :comments))
+        # render plain: @task.inspect
+        @task.save
+        redirect_to task_path(@task)
+        # 
+        
+    end
+    
 end
