375726fc4c8ca79c262826be69fde21cdc718bdd
Mahe2809 <mahe2809pro@gmail.com>
2020-06-30 12:08:32 +0530

    Update and field validation fields included.

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -7,15 +7,34 @@ class TaskController < ApplicationController
         @tasks = Task.all
     end
     def new
-
+        # @task = Task.new #This can also be done to avoid empty values in the new file.
     end
     def create
         @task = Task.new(params.require(:task).permit(:task, :comments))
-        # render plain: @task.inspect
-        @task.save
-        redirect_to task_path(@task)
-        # 
-        
+
+        if @task.save
+            flash[:notice]= "Article created successfully" #Appears only when the task is created.
+            redirect_to task_path(@task)    
+        else
+            render 'new'
+        end
+    end
+
+    def edit 
+        @task = Task.find(params[:id]) #This can also be done to avoid empty values in the new file.
+    end
+
+    def update
+        @task = Task.find(params[:id])
+
+        @task.update(params.require(:task).permit(:task, :comments))
+
+        if @task.save
+            flash[:notice]= "Article updated successfully" #Appears only when the task is created.
+            redirect_to task_path(@task)    
+        else
+            render 'new'
+        end
     end
     
 end
