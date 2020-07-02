3a64deba3e818f917d2113b50043789dbe075f5a
Mahe2809 <mahe2809pro@gmail.com>
2020-07-02 11:40:01 +0530

    Introducing created at and updated at fields along with password bcrypting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+    before_save { self.email = email.downcase }
     has_many :tasks
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -10,4 +11,5 @@ class User < ApplicationRecord
         length: {maximum: 150}, 
         uniqueness: {case_sensitive: false},
         format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_123734) do
+ActiveRecord::Schema.define(version: 2020_07_02_055258) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_07_01_123734) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
